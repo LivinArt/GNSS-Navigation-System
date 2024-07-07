a35178a8b812960ba909c35173c5f0b93b840e81
vaishnavihole <vaishnavihole1@gmail.com>
2024-07-07 20:49:18 +0530

    fix bug

--- client/src/components/GnnsRxNestedMenu/GnnsRxItem.js
+++ client/src/components/GnnsRxNestedMenu/GnnsRxItem.js
@@ -1,7 +1,7 @@
 const MENUS = [
   {
     label: "GNNS-SDR",
-    onClickUrl: ("http://127.0.0.1/script/api/1.php", "_self"),
+    onClickUrl: "http://127.0.0.1/script/api/1.php",
   },
   {
     label: "NavIC",
